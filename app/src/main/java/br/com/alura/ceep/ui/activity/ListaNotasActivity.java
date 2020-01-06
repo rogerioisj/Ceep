141a2dab65f9dac478bd1b76a780952e8c834cac
roger <rogerio.reaper@gmail.com>
2020-01-06 01:24:01 -0300

    Initial commit

---
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -0,0 +1,34 @@
+package br.com.alura.ceep.ui.activity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import br.com.alura.ceep.R;
+import br.com.alura.ceep.dao.NotaDAO;
+import br.com.alura.ceep.model.Nota;
+import br.com.alura.ceep.recyclerview.adapter.ListaNotasAdapter;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+        RecyclerView listaNotas = findViewById(R.id.lista_notas_recyclerview);
+        NotaDAO dao = new NotaDAO();
+        for(int i=1; i<=10000; i++){
+            dao.insere(new Nota("Titulo " +i, "Descrição " +i));
+        }
+        List<Nota> todasNotas = dao.todos();
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        listaNotas.setLayoutManager(layoutManager);
+
+    }
+}
